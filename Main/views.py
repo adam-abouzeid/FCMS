38c386a8ee7b9fbf9c0cda249889d1ffa13d02f8
Mohammad Adel Alsoheil <alsoheilm@gmail.com>
2024-04-07 04:18:22 +0300

    created models for users with relative permissions and the news updated the navbar for the layout and started implementing the horizontal navbar for the news in the main page aka index

--- Main/views.py
+++ Main/views.py
@@ -4,8 +4,12 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from .models import *
+from .util import *
+
 # Create your views here.
 
 
 def index(request):
-    return render(request,"Main/index.html")
+    return render(request,"Main/index.html",{
+        "News": get_News()
+    })
