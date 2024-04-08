4a4a079dba1c757d86e7cc1e3b1e33156b25d52b
Mohammad Adel Alsoheil <alsoheilm@gmail.com>
2024-04-08 14:40:50 +0300

    Added news frontend design

--- Authentication/models.py
+++ Authentication/models.py
@@ -3,19 +3,20 @@ from django.db import models
 from django.utils import timezone
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from Main.models import *
+
 
 # Create your models here.
 # here are the databases that we are going to use for the website
 
 
 class User(AbstractUser):
+
     class Role(models.TextChoices):
         ADMIN = "ADMIN", 'Admin'
         FAN = "FAN", 'Fan'
         CLUB_STAFF = "CLUB_STAFF", 'Club_Staff'
 
-    base_role = Role.ADMIN #since if a fan or club staff needs to register we can handle their registration manually to not be an admin
+    base_role = Role.FAN #since if a fan or club staff needs to register we can handle their registration manually to not be an admin
     profile_picture = models.ImageField(upload_to='profile_pics', null=True, blank=True)
     
     role = models.CharField(max_length=50, choices=Role.choices)
@@ -28,13 +29,15 @@ class User(AbstractUser):
             self.role = self.base_role
             return super().save(*arg,**kwargs)
         
+"""
         
+#remove       
 class FanManager(BaseUserManager): #filter only fans when you query data
     def get_queryset(self, *args, **kwargs):
         results = super().get_queryset(*args,**kwargs)
         return results.filter(role=User.Role.FAN)
 
-        
+   #remove         
 class Fan(User): #this is a proxy model it wont be generated it justs makes the process of seperation easier
 
     base_role = User.Role.FAN
@@ -42,25 +45,25 @@ class Fan(User): #this is a proxy model it wont be generated it justs makes the 
 
     class Meta:
         proxy = True
-
+#remove    
 @receiver(post_save, sender=Fan)
 def create_user_profile(sender, instance, created, **kwargs):
     if created and instance.role == "FAN":
         FanProfile.objects.create(user=instance)
 
 
-        
+  #remove          
 class FanProfile(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE)
     fan_id = models.IntegerField(null=True, blank=True)
 
-
+  
 class ClubStaffManager(BaseUserManager): #filter only club staff when you query data
     def get_queryset(self, *args, **kwargs):
         results = super().get_queryset(*args,**kwargs)
         return results.filter(role=User.Role.CLUB_STAFF)
 
-        
+            
 class Club_Staff(User): #this is a proxy model it wont be generated it justs makes the process of seperation easier
 
     base_role = User.Role.CLUB_STAFF
@@ -68,20 +71,20 @@ class Club_Staff(User): #this is a proxy model it wont be generated it justs mak
 
     class Meta:
         proxy = True
-
+#remove    
 @receiver(post_save, sender=Club_Staff)
 def create_user_profile(sender, instance, created, **kwargs):
     if created and instance.role == "CUB_STAFF":
         Club_Staff_Profile.objects.create(user=instance)
 
 
-        
+  #remove          
 class Club_Staff_Profile(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE)
     club_staff_id = models.IntegerField(null=True, blank=True)
 
 
-    
+"""
     
 
 
