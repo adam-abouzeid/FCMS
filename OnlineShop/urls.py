cec7161c5cad741525b6bde1013a29060b322c83
ahmadabbass11 <ahmadabs183@gmail.com>
2024-04-19 22:42:31 +0300

    Finalized work on shop

--- OnlineShop/urls.py
+++ OnlineShop/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-      path("FanShop", views.Shop_Main, name="Shop_Main"),
-      path('product/<int:product_id>/', views.product_list, name='product_list'),
-      path('add-to-cart/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
-      path('remove-from-cart/<int:order_item_id>/', views.remove_from_cart, name='remove_from_cart'),
+      path("Shop_Main/",views.Shop_Main, name="Shop_Main"),
+      path('add-to-cart/<str:name>/', views.add_to_cart, name='add_to_cart'),
+      path('remove-from-cart', views.remove_from_cart, name='remove_from_cart'),
       path('cart/', views.cart, name='cart'),
       path('checkout/', views.checkout, name='checkout'),
       path('request-refund/', views.request_refund, name='request_refund'),
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-]
